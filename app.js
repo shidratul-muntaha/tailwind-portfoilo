237b42d80ebeff3bb8b77950041f5dffc5bb1267
Shidratul-Muntaha <155138596+shidratul-muntaha@users.noreply.github.com>
2024-03-04 22:06:51 +0600

    Add somecode

--- app.js
+++ app.js
@@ -34,3 +34,69 @@ async function sendEmail(params, e) {
    }
 }
 
+
+
+
+class TextAnimator {
+   constructor(selector, options) {
+       this.text = document.querySelector(selector);
+       this.strText = this.text.textContent.trim();
+       this.splitText = this.strText.split("");
+       this.text.textContent = "";
+       this.options = options || {};
+       this.margin = this.options.margin || '0px';
+       this.delay = this.options.delay || 0;
+       this.class = this.options.class || 'text-span';
+   }
+
+   animate() {
+       for (let i = 0; i < this.splitText.length; i++) {
+           if (this.splitText[i] === " ") {
+               this.text.innerHTML += "&nbsp;";
+           } else {
+               this.text.innerHTML += "<span class='"+ this.class + "' style='margin-right:" + this.margin + ";animation-delay:" + (i * this.delay) + "ms;'><span class='fade-in' style='animation-delay:" + (i * this.delay) + "ms;'>" + this.splitText[i] + "</span></span>";
+           }
+       }
+   }
+   
+}
+
+const animator = new TextAnimator('.text-blob', {
+  
+   delay: 100,
+   class: "text-blob__letter"
+});
+animator.animate();
+
+
+
+
+const words = ["Freelancer", "Web Developer", "Coder","Designer", "Programmar"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
